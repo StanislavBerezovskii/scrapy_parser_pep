aa1e24ba75025335b27f5444ec89ab9237114b99
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2023-05-21 12:21:23 +0200

    SPRINT19, Berezovskii, Initial

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,26 @@
+import csv
+from collections import Counter
+from datetime import datetime as dt
+from scrapy.exceptions import DropItem
+
+from pep_parse.settings import BASE_DIR, FILENAME, RESULTS_DIR, TIME_FORMAT
+
+
+class PepParsePipeline:
+    def open_spider(self, spider):
+        self.total = Counter()
+        self.time = dt.now().strftime(TIME_FORMAT)
+
+    def process_item(self, item, spider):
+        self.total[item['status']] += 1
+        if 'status' not in item:
+            raise DropItem('Status не найден')
+        return item
+
+    def close_spider(self, spider):
+        results_dir = BASE_DIR / RESULTS_DIR
+        file_path = results_dir / FILENAME.format(self.time)
+        file = csv.writer(open(file_path, 'w'))
+        file.writerow(['Статус', 'Количество'])
+        self.total['Total'] = sum(self.total.values())
+        file.writerows(self.total.items())
---
+++ pep_parse/settings.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+
+BOT_NAME = "pep_parse"
+
+SPIDER_MODULES = ["pep_parse.spiders"]
+NEWSPIDER_MODULE = "pep_parse.spiders"
+
+ROBOTSTXT_OBEY = True
+
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
+
+BASE_DIR = Path(__file__).parent.parent
+FILENAME = 'status_summary_{}.csv'
+PEP_REGEXP = r'PEP\s(?P<number>\d+)\W+(?P<name>.+)$'
+RESULTS_DIR = 'results'
+TIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+
+FEEDS = {
+    'results/pep_%(time)s.csv': {
+        'format': 'csv',
+        'fields': ['number', 'name', 'status'],
+        'overwrite': True,
+    }
+}
+ITEM_PIPELINES = {
+    'pep_parse.pipelines.PepParsePipeline': 300,
+}
